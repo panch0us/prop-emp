378262668bcae62dd09fe27eb72c4d88807fc620
panch0us <godbryansk@yandex.ru>
2021-11-12 21:53:38 +0300

    Изменено описание и в модуль db.py добавлен тестовый вариант создания таблицы

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 # Модуль для создания базы данных для учета имущества
+import sqlite3
+
 
 def exist_db():
     """Проверяем существует база данных?"""
@@ -7,7 +9,23 @@ def exist_db():
 
 def create_db():
     """Создание базы данных"""
-    pass
+    # Создаем файл базы данных. Если он уже создан - подключаемся.
+    conn = sqlite3.connect('property-accounting.db')
+    # Создаем объект курсора для выполнения запросов
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS employee
+    (id INTEGER PRIMARY KEY,
+    surname   TEXT    NOT NULL,
+    name   TEXT    NOT NULL,
+    middle_name   TEXT    NOT NULL)''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS property
+    (p_id INTEGER PRIMARY KEY,
+    title TEXT    NOT NULL,
+    type    TEXT    NOT NULL,
+    id INT,
+    FOREIGN KEY (id) REFERENCES employee (id))''')
+    conn.commit()
 
 
 if __name__ == '__main__':
