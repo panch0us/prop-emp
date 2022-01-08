e1ae4a8533d09669c1464cfc95c97f87c8175b0c
panch0us <godbryansk@yandex.ru>
2022-01-08 21:19:09 +0300

    Тестовое отображение страницы (вывод дней рождений сотрудников в текущем месяце)

--- web/ic/views.py
+++ web/ic/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Employees
+
+from datetime import datetime
 
 
 def index(request):
-    return HttpResponse("Главная страница")
+    """Главная страница"""
+    # Выбираем список сотрудников, у кого в текущем месяце день рождения
+    employees = Employees.objects.all()
+    current_month = datetime.now().month
+    return render(request, 'ic/index.html', {'employees': employees,
+                                             'current_month': current_month})
+
