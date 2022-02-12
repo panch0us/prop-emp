da371f1d37ce64c393f38953fd57181dc423eada
panch0us <godbryansk@yandex.ru>
2022-02-12 22:32:56 +0300

    функция заменена классом

--- web/ic/views.py
+++ web/ic/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+# Ниже одна строка импортируется для класса EmployeesBirthMonth
+from django.views.generic.base import View
 from .models import Employees
 
 from datetime import datetime
 
 
-def index(request):
-    """Главная страница"""
-    # Выбираем список сотрудников, у кого в текущем месяце день рождения
-    employees = Employees.objects.all()
-    current_month = datetime.now().month
-    return render(request, 'ic/index.html', {'employees': employees,
-                                             'current_month': current_month})
+class EmployeesBirthMonth(View):
+    """Месяц рождения сотрудников"""
+    def get(self, request):
+        """Главная страница"""
+        # Выбираем список сотрудников, у кого в текущем месяце день рождения
+        employees = Employees.objects.all()
+        current_month = datetime.now().month
+        return render(request, 'ic/employees.html', {'employees': employees, 'current_month': current_month})
 
