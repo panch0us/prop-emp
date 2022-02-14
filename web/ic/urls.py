9c9113bea968299fcdbb67de7d55730c58ad7c32
panch0us <godbryansk@yandex.ru>
2022-02-14 12:14:26 +0300

    добавлен новый путь ic/slug для построения url к конкретному пользователю

--- web/ic/urls.py
+++ web/ic/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.EmployeesBirthMonth.as_view())
+    path('', views.EmployeesView.as_view()),
+    path('<slug:slug>/', views.EmployeesDetailView.as_view(), name='employee_detail')
 ]
