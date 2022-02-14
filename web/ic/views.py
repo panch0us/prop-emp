704637caa635a87f53df06a95d79c9e952f63ba7
panch0us <godbryansk@yandex.ru>
2022-02-14 12:13:30 +0300

    views изменены функции на классы

--- web/ic/views.py
+++ web/ic/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
-# Ниже одна строка импортируется для класса EmployeesBirthMonth
-from django.views.generic.base import View
+
+from django.views.generic import ListView, DetailView
+
 from .models import Employees
 
 from datetime import datetime
 
 
-class EmployeesBirthMonth(View):
-    """Месяц рождения сотрудников"""
-    def get(self, request):
-        """Главная страница"""
-        # Выбираем список сотрудников, у кого в текущем месяце день рождения
-        employees = Employees.objects.all()
-        current_month = datetime.now().month
-        return render(request, 'ic/employees.html', {'employees': employees, 'current_month': current_month})
+class EmployeesView(ListView):
+    """Список сотрудников, подставляет к названию модели _list"""
+    model = Employees
+    queryset = Employees.objects.all()
+    current_month = datetime.now().month
+    context_object_name = 'employees_list'
 
+
+class EmployeesDetailView(DetailView):
+    """Полное описание сотрудника по url (slug)"""
+    model = Employees
+    slug_field = "emp_url"
+
+
+class Asd(ListView):
+    """Дни рождения сотрудников в текущем месяце (ДОРАБОТАТЬ ПОСЛЕ)"""
+    # current_month = datetime.now().month
+    pass
