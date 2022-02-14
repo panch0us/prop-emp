01aad3ce1d9d597cc1689b087a8be0f6b853a4d1
panch0us <godbryansk@yandex.ru>
2022-02-14 12:09:14 +0300

    удален ненужный импорт

--- web/ic/admin.py
+++ web/ic/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-# Импортирую группы для разграничения прав просмотра в Админке (тест)
-from django.contrib.auth.models import User, Group
 
 from .models import Cabinets, TypesWork, Divisions, DepartmentsFirst, DepartmentsSecond, Positions, EmployeesStatus, \
                     Employees, ComputersIsod, DiskStorageIsod, PropertyStandarts, Property, TypesProperty, Ranks, \
