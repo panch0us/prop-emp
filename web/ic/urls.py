19350ce943b91a29afb6a722c1093f4dbc4c60e4
panch0us <godbryansk@yandex.ru>
2022-02-15 16:54:49 +0300

    Добавлен путь к телефонной книге

--- web/ic/urls.py
+++ web/ic/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.EmployeesView.as_view()),
+    path('phone_book/', views.PhonebookView.as_view()),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('<slug:slug>/', views.EmployeesDetailView.as_view(), name='employee_detail')
+
 ]
