6856dd214be2527afde7bd5eda4d4199be991048
Pavel Savkin <59056623+panch0us@users.noreply.github.com>
2022-09-14 20:22:32 +0300

    Update output.sql

--- SQL/output.sql
+++ SQL/output.sql
@@ -23,11 +23,9 @@ DELIMITER E'\t'
 NULL AS ''
 CSV;
 
-
-
 /* 
 ********************************************************************************************************
-Вывод списка АРМ с МНИ для ЦИТСиЗИ (старый вариант для образцы)
+Вывод списка АРМ с МНИ для ЦИТСиЗИ (старый вариант для образца)
 ********************************************************************************************************
 */
 
@@ -36,30 +34,30 @@ SELECT
 	public.computers_isod.comp_reg_num																AS "№ п/п",
 	public.computers_isod.comp_factory_num															AS "Заводской номер АРМ",
 	array_to_string(array_agg(public.disk_storage_isod.disk_model	|| '/' || 	
-							  public.disk_storage_isod.disk_size), E';\n')							AS "Модель МНИ/ объем",
-	array_to_string(array_agg(public.disk_storage_isod.disk_factory_num), E';\n')					AS "Заводской номер МНИ",
-	array_to_string(array_agg(public.disk_storage_isod.disk_reg_num), E';\n')						AS "Регистрационный номер МНИ",
+							  public.disk_storage_isod.disk_size), E';\n')	AS "Модель МНИ/ объем",
+	array_to_string(array_agg(public.disk_storage_isod.disk_factory_num), E';\n')			AS "Заводской номер МНИ",
+	array_to_string(array_agg(public.disk_storage_isod.disk_reg_num), E';\n')			AS "Регистрационный номер МНИ",
 	public.computers_isod.comp_mac_address 															AS "МАС адрес сетевой карты",
 	public.computers_isod.comp_ip_address 															AS "IP-адрес",
 	public.computers_isod.comp_virt_ip_address 														AS "Виртуальный IP-адрес",
 	public.computers_isod.comp_id_dst_file 															AS "ID  DST", 
 	public.computers_isod.comp_title_dst_file 														AS "Имя DST", 
-	'г. Брянск, пр-т Ленина д. 18, кабинет № ' || public.cabinets.cab_num							AS "Адрес дислокации АРМ",
+	'г. Брянск, пр-т Ленина д. 18, кабинет № ' || public.cabinets.cab_num				AS "Адрес дислокации АРМ",
 	public.employees.emp_surname			|| ' ' ||
 		public.employees.emp_name			|| ' ' ||
 		public.employees.emp_middle_name 	|| ', '	||
 		public.positions.pos_title																	AS "Ф.И.О., должность пользователя", 
-	public.computers_isod.comp_attestation_status 													AS "Состояние аттестации АРМ"
+	public.computers_isod.comp_attestation_status 							AS "Состояние аттестации АРМ"
 FROM 
 	public.computers_isod
 	-- Присоеденяем таблицу "disk_storage_isod"
 	LEFT OUTER JOIN public.disk_storage_isod 	ON public.computers_isod.comp_id = public.disk_storage_isod.fk_install_in_comp
 	-- Присоеденяем таблицу "cabinets"
-	LEFT OUTER JOIN public.cabinets 			ON public.computers_isod.fk_cabinet_location = public.cabinets.cab_id
+	LEFT OUTER JOIN public.cabinets 		ON public.computers_isod.fk_cabinet_location = public.cabinets.cab_id
 	-- Присоеденяем таблицу "employees"
-	LEFT OUTER JOIN public.employees 			ON public.computers_isod.fk_comp_owner = public.employees.emp_id
+	LEFT OUTER JOIN public.employees 		ON public.computers_isod.fk_comp_owner = public.employees.emp_id
 	-- Присоеденяем таблицу "positions"
-	LEFT OUTER JOIN public.positions 			ON public.employees.fk_position = public.positions.pos_id
+	LEFT OUTER JOIN public.positions 		ON public.employees.fk_position = public.positions.pos_id
 GROUP BY
 	public.computers_isod.comp_id,	
 	public.cabinets.cab_id,
@@ -76,12 +74,12 @@ CSV;
 Выбор списка подразделений, отделов и отделений
 *********************************************************************************************************/
 SELECT
-	div_title 			AS "Подразделение",
+	div_title 		AS "Подразделение",
 	dep_first_title		AS "Отдел",
 	dep_second_title	AS "Отделение"
 FROM public.divisions
-	LEFT OUTER JOIN public.departments_first ON public.divisions.div_id = public.departments_first.fk_div
-	LEFT OUTER JOIN public.departments_second ON public.departments_first.dep_first_id = public.departments_second.fk_dep_first
+	LEFT OUTER JOIN public.departments_first 	ON public.divisions.div_id = public.departments_first.fk_div
+	LEFT OUTER JOIN public.departments_second 	ON public.departments_first.dep_first_id = public.departments_second.fk_dep_first
 
 
 /*********************************************************************************************************
